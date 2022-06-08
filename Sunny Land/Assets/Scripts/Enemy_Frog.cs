037b126193bd23da679b966690f26a083539f6c2
Von-Vam <16686jdj@gmail.com>
2022-06-08 16:18:54 -0400

    Frog Enemy AI

---
+++ Sunny Land/Assets/Scripts/Enemy_Frog.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Frog : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public Transform leftpoint, rightpoint;
+    public float Speed;
+
+    private bool Faceleft = true;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.DetachChildren();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement () 
+    {
+        if (Faceleft)
+        {
+            rb.velocity = new Vector2(-Speed, rb.velocity.y);
+            if (transform.position.x < leftpoint.position.x)
+            {
+                transform.localScale = new Vector3(-1,1,1);
+                Faceleft = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(Speed, rb.velocity.y);
+            if (transform.position.x > rightpoint.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                Faceleft = true;
+            }
+                
+        }
+    
+    }
+}
