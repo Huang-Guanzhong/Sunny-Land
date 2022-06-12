a2ac3689ed40ab830aabc97a795ed12d56e57b35
Von-Vam <16686jdj@gmail.com>
2022-06-12 15:22:13 -0400

    Enemy_Eagle_1.0

--- Sunny Land/Assets/Scripts/Enemy_Eagle.cs
+++ Sunny Land/Assets/Scripts/Enemy_Eagle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy_Eagle : MonoBehaviour
 {
     private Rigidbody2D rb;
+<<<<<<< Updated upstream
 
     public Transform uppoint, downpoint;
     public float Speed;
@@ -19,6 +20,25 @@ public class Enemy_Eagle : MonoBehaviour
         downy = downpoint.position.y;
         Destroy(uppoint.gameObject);
         Destroy(downpoint.gameObject);
+=======
+    private Collider2D coll;
+    public Transform top;
+    public Transform bottom;
+    public float Speed;
+    public float TopY;
+    public float BottomY;
+
+    private bool isUp;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        TopY = top.position.y;
+        BottomY = bottom.position.y;
+        Destroy(top.gameObject);
+        Destroy(bottom.gameObject);
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -27,6 +47,7 @@ public class Enemy_Eagle : MonoBehaviour
         Movement();
     }
 
+<<<<<<< Updated upstream
     void Movement()
     {
         if (Faceup)
@@ -49,5 +70,26 @@ public class Enemy_Eagle : MonoBehaviour
 
         }
 
+=======
+    void Movement ()
+    {
+        if (isUp)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Speed);
+            if (transform.position.y > TopY)
+            {
+                isUp = false;
+            }
+        }
+
+        else
+        {
+            rb.velocity = new Vector2 (rb.velocity.x, -Speed);
+            if (transform.position.y < BottomY)
+            {
+                isUp = true;
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
