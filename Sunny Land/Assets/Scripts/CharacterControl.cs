4fe5fdab8b8e17a6da4f7deaeb63d8b61e49dd23
Von-Vam <16686jdj@gmail.com>
2022-06-23 00:03:33 -0400

    Switch Back to PC Version

--- Sunny Land/Assets/Scripts/CharacterControl.cs
+++ Sunny Land/Assets/Scripts/CharacterControl.cs
@@ -47,9 +47,9 @@ public class CharacterControl : MonoBehaviour
         SwitchAnim();
     }
 
-    /*void Movement() //Movement on PC
+    void Movement() //Movement on PC
     {
-        float HorizontalMove = Input.GetAxis("Horizontal"); PC Control
+        float HorizontalMove = Input.GetAxis("Horizontal");
         float facedirection = Input.GetAxisRaw("Horizontal");
 
         //chareacter movement
@@ -63,169 +63,169 @@ public class CharacterControl : MonoBehaviour
         {
             transform.localScale = new Vector3(facedirection, 1, 1);
         
-        }*/
-
-     void Movement() //Movement on Mobile
-    {
-        float HorizontalMove = joystick.Horizontal;//-1f~1f
-        float facedirection = joystick.Horizontal;
-
-        //chareacter movement
-        if (HorizontalMove != 0f)
-        {
-            rb.velocity = new Vector2(HorizontalMove * speed, rb.velocity.y);
-            anim.SetFloat("Running", Mathf.Abs(facedirection));
         }
 
-        if (facedirection > 0f)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
+        /*void Movement() //Movement on Mobile
+       {
+           float HorizontalMove = joystick.Horizontal;//-1f~1f
+           float facedirection = joystick.Horizontal;
 
-        }
-        if (facedirection < 0f)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
+           //chareacter movement
+           if (HorizontalMove != 0f)
+           {
+               rb.velocity = new Vector2(HorizontalMove * speed, rb.velocity.y);
+               anim.SetFloat("Running", Mathf.Abs(facedirection));
+           }
 
-        }
+           if (facedirection > 0f)
+           {
+               transform.localScale = new Vector3(1, 1, 1);
+
+           }
+           if (facedirection < 0f)
+           {
+               transform.localScale = new Vector3(-1, 1, 1);
+
+           }*/
 
 
         //character jump on PC
-        /*if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            JumpAudio.Play();
+            anim.SetBool("Jumping", true);
+        }
+
+        //character jump on Mobile
+        /*if (joystick.Vertical > 0.5f && coll.IsTouchingLayers(ground))//-1f~1f
         {
             rb.velocity = new Vector2(rb.velocity.x, JumpForce);
             JumpAudio.Play();
             anim.SetBool("Jumping", true);
         }*/
 
-        //character jump on Mobile
-        if (joystick.Vertical > 0.5f && coll.IsTouchingLayers(ground))//-1f~1f
+        Crouch();
+
+}
+
+//Switch Animations
+void SwitchAnim() 
+{
+    anim.SetBool("Idle", false);
+
+    if (rb.velocity.y < 0.1f && !coll.IsTouchingLayers(ground))
+    {
+        anim. SetBool("Falling", true) ;
+    }
+
+    if (anim.GetBool("Jumping"))
+    {
+        if (rb.velocity.y < 0)
         {
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Falling", true);
+        }
+    }
+
+    else if (isHurt)
+    {
+        anim.SetBool("Hurt", true);
+        anim.SetFloat("Running", 0);
+        if (Mathf.Abs(rb.velocity.x) < 0.3f) 
+        {
+            anim.SetBool("Hurt", false);
+            anim.SetBool("Idle", true);
+            isHurt = false;
+        }
+    }
+
+    else if (coll.IsTouchingLayers(ground))
+    {
+        anim.SetBool("Falling", false);
+        anim.SetBool("Idle", true);
+    }
+
+}
+
+//Collider Trigger
+private void OnTriggerEnter2D(Collider2D collision)
+{
+    //Cherry Collections
+    if (collision.tag == "Cherry")
+    {
+        CherryAudio.Play();
+        Destroy(collision.gameObject);
+        Cherry += 1;
+        CherryNum.text = Cherry.ToString();
+    }
+
+    //Gem Collections
+    if (collision.tag == "Gem")
+    {
+        GemAudio.Play();
+        Destroy(collision.gameObject);
+        Gem += 1;
+        GemNum.text = Gem.ToString();
+    }
+
+    if (collision.tag == "DeadLine")
+    {
+        GetComponent<AudioSource>().enabled = false;
+        Invoke("Restart", 2f);
+    }
+}
+
+//Elimate Targets
+private void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.tag == "Enemy")
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (anim.GetBool("Falling") && transform.position.y > (collision.gameObject.transform.position.y + 0.5f))
+        {
+            enemy.JumpOn();
             rb.velocity = new Vector2(rb.velocity.x, JumpForce);
-            JumpAudio.Play();
             anim.SetBool("Jumping", true);
         }
-
-        Crouch();
-    
+        //Hurt
+        else if (transform.position.x < collision.gameObject.transform.position.x)
+        {
+            rb.velocity = new Vector2(-8, rb.velocity.y);
+            HurtAudio.Play();
+            isHurt = true;
+        }
+        else if (transform.position.x > collision.gameObject.transform.position.x)
+        {
+            rb.velocity = new Vector2(8, rb.velocity.y);
+            HurtAudio.Play();
+            isHurt = true;
+        }
     }
+}
 
-    //Switch Animations
-    void SwitchAnim() 
+//Crouch on PC
+void Crouch()
+{
+    if (!Physics2D.OverlapCircle(CeilingCheck.position, 0.2f, ground))
     {
-        anim.SetBool("Idle", false);
-
-        if (rb.velocity.y < 0.1f && !coll.IsTouchingLayers(ground))
+        if (Input.GetButton("Crouch"))
         {
-            anim. SetBool("Falling", true) ;
+            anim.SetBool("Crouching", true);
+            DisColl.enabled = false;
         }
 
-        if (anim.GetBool("Jumping"))
+        else
         {
-            if (rb.velocity.y < 0)
-            {
-                anim.SetBool("Jumping", false);
-                anim.SetBool("Falling", true);
-            }
-        }
-
-        else if (isHurt)
-        {
-            anim.SetBool("Hurt", true);
-            anim.SetFloat("Running", 0);
-            if (Mathf.Abs(rb.velocity.x) < 0.3f) 
-            {
-                anim.SetBool("Hurt", false);
-                anim.SetBool("Idle", true);
-                isHurt = false;
-            }
-        }
-
-        else if (coll.IsTouchingLayers(ground))
-        {
-            anim.SetBool("Falling", false);
-            anim.SetBool("Idle", true);
-        }
-    
-    }
-
-    //Collider Trigger
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //Cherry Collections
-        if (collision.tag == "Cherry")
-        {
-            CherryAudio.Play();
-            Destroy(collision.gameObject);
-            Cherry += 1;
-            CherryNum.text = Cherry.ToString();
-        }
-
-        //Gem Collections
-        if (collision.tag == "Gem")
-        {
-            GemAudio.Play();
-            Destroy(collision.gameObject);
-            Gem += 1;
-            GemNum.text = Gem.ToString();
-        }
-
-        if (collision.tag == "DeadLine")
-        {
-            GetComponent<AudioSource>().enabled = false;
-            Invoke("Restart", 2f);
+            anim.SetBool("Crouching", false);
+            DisColl.enabled = true;
         }
     }
 
-    //Elimate Targets
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-            if (anim.GetBool("Falling") && transform.position.y > (collision.gameObject.transform.position.y + 0.5f))
-            {
-                enemy.JumpOn();
-                rb.velocity = new Vector2(rb.velocity.x, JumpForce);
-                anim.SetBool("Jumping", true);
-            }
-            //Hurt
-            else if (transform.position.x < collision.gameObject.transform.position.x)
-            {
-                rb.velocity = new Vector2(-8, rb.velocity.y);
-                HurtAudio.Play();
-                isHurt = true;
-            }
-            else if (transform.position.x > collision.gameObject.transform.position.x)
-            {
-                rb.velocity = new Vector2(8, rb.velocity.y);
-                HurtAudio.Play();
-                isHurt = true;
-            }
-        }
-    }
-
-    //Crouch on PC
-    /*void Crouch()
-    {
-        if (!Physics2D.OverlapCircle(CeilingCheck.position, 0.2f, ground))
-        {
-            if (Input.GetButton("Crouch"))
-            {
-                anim.SetBool("Crouching", true);
-                DisColl.enabled = false;
-            }
-
-            else
-            {
-                anim.SetBool("Crouching", false);
-                DisColl.enabled = true;
-            }
-        }
-        
-    }*/
+}
 
     //Crouch on Mobile
-    void Crouch()
+    /*void Crouch()
     {
         if (!Physics2D.OverlapCircle(CeilingCheck.position, 0.2f, ground))
         {
@@ -242,11 +242,11 @@ public class CharacterControl : MonoBehaviour
             }
         }
 
-    }
+    }*/
 
     void Restart()
-    {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+{
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
 }
 
